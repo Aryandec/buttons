c6d7b652055c18e069e7a0bf05cf636e7ece13a5
Aryandec <aryanbh480@gmail.com>
2025-05-29 22:39:23 +0530

    twersks2

--- app/results/page.js
+++ app/results/page.js
@@ -1,30 +1,33 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { useEffect, useState } from "react";
 
 export default function ResultsPage() {
   const searchParams = useSearchParams();
-  const count = searchParams.get("count") || 0;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Retrieve the count value from searchParams only on the client side
+    const countValue = searchParams.get("count") || 0;
+    setCount(countValue);
+  }, [searchParams]);
 
   return (
-    <Suspense fallback={<div className="text-white">Loading...</div>}>
-      <div className="flex items-center justify-center h-screen bg-[#1a1a1a]">
-        <div className="text-center p-8 bg-gray-800 rounded-lg shadow-lg">
-          <h1 className="text-4xl font-bold text-white mb-4">
-            You think you will have {count} girlfriend(s)?
-          </h1>
-          <h2 className="text-2xl text-gray-600 mb-6">
-            Reality might be different 😢 <br /> This is the curse of this
-            website!
-          </h2>
-          <button
-            className="btn btn-primary px-6 py-3 text-white font-bold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
-            onClick={() => window.history.back()}
-          >
-            Go Back
-          </button>
-        </div>
+    <div className="flex items-center justify-center h-screen bg-[#1a1a1a]">
+      <div className="text-center p-8 bg-gray-800 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          You think you will have {count} girlfriend(s)?
+        </h1>
+        <h2 className="text-2xl text-gray-600 mb-6">
+          Reality might be different 😢 <br /> This is the curse of this website!
+        </h2>
+        <button
+          className="btn btn-primary px-6 py-3 text-white font-bold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
+          onClick={() => window.history.back()}
+        >
+          Go Back
+        </button>
       </div>
-    </Suspense>
+    </div>
   );
 }
